8bf007c6d9733dd38c360a8ec3e378290cb9593b
AmirMurmansk Astra <Amirastra78@gmail.com>
2023-07-04 22:03:34 +0300

    Saved new test home work

---
+++ src/test/java/CalculateSimpleTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+
+public class CalculateSimpleTest {
+
+
+    @org.junit.jupiter.api.Test
+    void sumTest() {
+        var testSum = new CalculateSimple();
+        Assertions.assertEquals(5, testSum.sum(2, 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void divisionTest() {
+        var testDivision = new CalculateSimple();
+        Assertions.assertEquals(2, testDivision.division(6, 3));
+        Assertions.assertEquals(Integer.MIN_VALUE,testDivision.division(10, 0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicationTest() {
+        var testMultiplication = new CalculateSimple();
+        Assertions.assertEquals(15, testMultiplication.multiplication(3, 5));
+    }
+}
